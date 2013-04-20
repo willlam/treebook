47477ab1e07608fc3f194164169a9ef9a52ecae5
Will Lam <will@willlam.net>
2013-04-20 13:36:45 -0400

    updated statuses controller test.rb

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,7 @@
 class StatusesController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update]
   # GET /statuses
-  # GET /statuses.json
+  # GET /statuses.json[]
   def index
     @statuses = Status.all
 
